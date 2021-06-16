94c5eb3abed17d1a11af35bb2e380869c86b82e3
zhangqingyu10 <zhangqingyu10@meituan.com>
2021-06-16 16:30:54 +0800

    add result saving functionality

--- src/haser-model_calculation.py
+++ src/haser-model_calculation.py
@@ -1,18 +1,24 @@
 # Top Level imports
 import numpy as np
 import astropy.units as u
+import pickle as pk
 from sbpy.activity import gas
 
-# Calculate the Haser model column density from 1 to  1e04  km for CO2  at 1 au, produced at a rate of  1e28 /s. Let the expansion velocity be 0.8 km/s.
 Q = 1e28 / u.s  # CO2 production rate
 v = 0.8 * u.km / u.s  # gas radial expansion speed
 
 rh = 1.0 * u.au  # heliocentric distance
 tau = gas.photo_timescale('CO2', source='CE83') # Crovisier & Encrenaz 1983
-gamma = v * tau * (rh / u.au)**2
-print('lengthscale = {:.0g}\n'.format(gamma))
+rho = np.logspace(4, 7) * u.km
 
-co2 = gas.Haser(Q, v, gamma)
+res = {'rho': rho}
 
-for rho in 10**np.arange(0, 5) * u.km:  # projected radial distance from the nucleus
-    print('{:10.1f} {:10.2e}'.format(rho, co2.column_density(rho)))
+data = []
+for rh in np.arange(1, 11, 2) * u.au:
+    gamma = v * tau * (rh / u.au)**2
+    co2 = gas.Haser(Q, v, gamma)
+    data.append({'rh' : rh, 'cds': co2.column_density(rho)})
+res['data'] = data
+
+with open('haser_ds', 'wb') as f:
+    pk.dump(res, f)
--- src/plot_haser-model-calculatoin.py
+++ src/plot_haser-model-calculatoin.py
@@ -1,22 +1,25 @@
 # plot the distribution of CO2
+from astropy.table import column
 import matplotlib.pyplot as plt
 import numpy as np
 import astropy.units as u
 from sbpy.activity import gas
 import pandas as pd
+import pickle as pk
+
 fig = plt.figure(1)
 fig.clear()
 ax = fig.gca()
 
-rho = np.logspace(4, 7) * u.km
-Q = 1e28 / u.s  # CO2 production rate
-v = 0.8 * u.km / u.s  # gas radial expansion speed
-tau = gas.photo_timescale('CO2', source='CE83') # Crovisier & Encrenaz 1983
+with open('haser_ds', 'rb') as f:
+    res = pk.load(f)
+rho = res['rho']
+data = res['data']
 
-for rh in np.arange(1, 11, 2) * u.au:
-    gamma = v * tau * (rh / u.au)**2
-    co2 = gas.Haser(Q, v, gamma)
-    ax.plot(rho, co2.column_density(rho), label='{:.1f}'.format(rh))
+for entry in data:
+    rh = entry['rh']
+    column_density = entry['cds']
+    ax.plot(rho, column_density, label='{:.1f}'.format(rh / u.au))
 
 plt.setp(ax, xlabel='Radial distance (km)', xscale='log',
          ylabel='Column density (m$^{-2}$)', ylim=(1e12, 1e18), yscale='log')
